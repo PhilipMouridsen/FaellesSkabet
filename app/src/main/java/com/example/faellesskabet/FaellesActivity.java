6ad9e3c610323370415b06dde6c2bc73499153fd
Philip Cronqvist Mouridsen <PhilipCM@Philips-MacBook-Pro-3.local>
2020-04-08 11:21:37 +0200

    v0

---
+++ app/src/main/java/com/example/faellesskabet/FaellesActivity.java
@@ -0,0 +1,14 @@
+package com.example.faellesskabet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FaellesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_Faelles);
+    }
+}
